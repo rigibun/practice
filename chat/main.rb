c30c06f3938bcea65c8240ade1ad8fb453965696
rigibun <mukojin.yoing@gmail.com>
2014-09-26 17:00:52 +0900

    clean

--- chat/main.rb
+++ chat/main.rb
@@ -1,6 +1,5 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
-require 'sinatra/cookies'
 require 'haml'
 require 'sass'
 
@@ -9,17 +8,6 @@ require_relative 'logic'
 class App < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
-  end
-
-  use Rack::Session::Cookie
-  helpers Sinatra::Cookies
-
-  get '/set' do 
-    cookies[:cookie] = 'test'
-  end
-
-  get '/get' do
-    puts cookies
   end
 
   get '/' do
