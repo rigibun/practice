8d9a8422fa6bff451a23286e7ba46c7e25e93b56
rigibun <mukojin.yoing@gmail.com>
2014-10-15 00:39:12 +0900

    /chat/ add class Room

--- chat/logic.rb
+++ chat/logic.rb
@@ -1,11 +1,32 @@
+require 'json'
+
 class Room
   def initialize title
     @title = title
-    @user = []
+    @users = []
+    @posts = []
+    @nextId = 1
   end
 
-  def login username, passward
-    if @user.find username
+  def join username
+    unless @users.find username
+      @users.push username
+      true
+    else
+      false
     end
+  end
+
+  def get id = 0
+    @posts[id .. -1]
+  end
+
+  def getJson id = 0
+    JSON.generate self.get id
+  end
+
+  def post user, message
+    @posts.push({user: user, message: message, time: Time.now})
+    @nextId += 1
   end
 end
